6862e494379b6d0206ce205ac9311790c7329104
mRcfps <1402491442@qq.com>
2019-04-12 20:33:56 +0800

    Add dataset prepare script

---
+++ config.py
@@ -0,0 +1,2 @@
+# Sampled patch size (also the input and output size of model)
+PATCH_SIZE = 256
---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .tile import divide_image_to_patches
+from .tile import combine_patches_to_image
